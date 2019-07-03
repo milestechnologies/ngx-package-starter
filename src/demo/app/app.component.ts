569ace73450270902901a71e06597f719f3e71a0
Chris Camac <ccamac07@gmail.com>
2019-07-03 08:01:39 -0400

    applying standards to demo

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -1,37 +1,71 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <div class="container">
-      <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a class="navbar-brand" routerLink="/home">Demo App</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" routerLink="/home">Home</a>
-            </li>
-          </ul>
+    selector: 'app-root',
+    template: `
+        <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+            <div class="container">
+                <div class="navbar-expand mr-auto navbar-nav">
+                    <a class="navbar-brand" routerLink="/home"
+                        >ngx-package-starter</a
+                    >
+                    <a
+                        class="nav-link"
+                        routerLink="/demo"
+                        routerLinkActive="active"
+                        >demo</a
+                    >
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </div>
+        </nav>
+        <br />
+        <div class="router container">
+            <router-outlet></router-outlet>
         </div>
-      </nav>
-    </div>
-    <br />
-    <div class="router container">
-      <router-outlet></router-outlet>
-    </div>
-  `,
+        <nav
+            class="navbar navbar-expand-sm navbar-dark bg-primary fixed-bottom"
+        >
+            <div class="container">
+                <div class="navbar-expand ml-auto navbar-nav">
+                    <div class="navbar-text">
+                        authored by
+                        <a
+                            target="_blank"
+                            href="https://www.milestechnologies.com/software-development"
+                            >Miles Technologies</a
+                        >
+                    </div>
+
+                    <a
+                        class="nav-link"
+                        href="https://github.com/milestechnologies"
+                        target="_blank"
+                    >
+                        <i class="fa fa-github"></i>
+                    </a>
+                    <a
+                        class="nav-link"
+                        href="https://twitter.com/milestech"
+                        target="_blank"
+                    >
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    `,
 })
 export class AppComponent {
-  constructor() {}
+    constructor() {}
 }
--- src/demo/app/demo.component.ts
+++ src/demo/app/demo.component.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     template: `
-        <h2>Demo Component</h2>
+        <div class="jumbotron demo-description-block">
+            <h4><em>How it works</em></h4>
+            <p>
+                messaging here about what to do and how it works. this should be
+                the major text on the page, with all other text and instructions
+                contained in comments in the code sample
+            </p>
+        </div>
+        <button class="btn btn-primary">
+            button here
+        </button>
+        <br />
+        <br />
+        [code sample goes here]
     `,
 })
 export class DemoComponent {
